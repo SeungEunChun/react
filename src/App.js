432f51c18a5eb5dcb3e244c49ad635e59296d89c
SeungEunChun <1000tmddms@gmail.com>
2023-12-11 12:45:20 +0900

    리액트 시작

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="/logo512.png" alt="logo"></img>
+        <p>컴포넌트?</p>
+        <p>컴포넌트란 컨텐츠를 공용화시켜 자바스크립트 파일로 분리시켜놓은것</p>
+        <p>소스호출하기 (public은 루트디렉토리로 / 하면 됩니다.)</p>
+        <p>src안에 넣은 소스는 node의 컴파일 과정을 거치므로</p>
+
       </header>
-    </div>
+    </div >
   );
 }
 
---
+++ src/layout/Hd.js
@@ -0,0 +1,12 @@
+function Hd() {
+    return (
+        <>
+            <p>내가 만든 컴포넌트</p>
+            <p>반드시 대문자로 시작해야하고, 함수 내에 return식이 있어야 한다.</p>
+            <p>반드시 export로 밖으로 내보내 주어야 한다.</p>
+            <p></p>
+        </>
+    )
+}
+
+export default Hd;
